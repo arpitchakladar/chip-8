f10b536669bad2ab3f3658a5d8d6b64a2c54395d
Arpit Chakladar <arpitchakladar@proton.me>
2026-03-27 22:21:56 +0530

    Finally able to run premade roms

--- internal/system/system.go
+++ internal/system/system.go
@@ -1,7 +1,11 @@
 package system
 
 import (
+	"fmt"
 	"os"
+	"time"
+
+	"github.com/veandco/go-sdl2/sdl"
 
 	"github.com/arpitchakladar/chip-8/internal/cpu"
 	"github.com/arpitchakladar/chip-8/internal/display"
@@ -55,4 +59,78 @@ func (s *System) Step() error {
 
 	// 3. Execute
 	return s.CPU.Execute(opcode, s.Memory, s.Display, s.Keyboard)
+}
+
+func (s *System) HandleKeyboard(event *sdl.KeyboardEvent) {
+	keyCode := event.Keysym.Sym
+	// Type 0x300 is KeyDown, 0x301 is KeyUp in SDL
+	isPressed := event.Type == sdl.KEYDOWN
+
+	// Explicitly cast constants to sdl.Keycode to satisfy the map type
+	mapping := map[sdl.Keycode]byte{
+		sdl.Keycode(sdl.K_1): 0x1, sdl.Keycode(sdl.K_2): 0x2, sdl.Keycode(sdl.K_3): 0x3, sdl.Keycode(sdl.K_4): 0xC,
+		sdl.Keycode(sdl.K_q): 0x4, sdl.Keycode(sdl.K_w): 0x5, sdl.Keycode(sdl.K_e): 0x6, sdl.Keycode(sdl.K_r): 0xD,
+		sdl.Keycode(sdl.K_a): 0x7, sdl.Keycode(sdl.K_s): 0x8, sdl.Keycode(sdl.K_d): 0x9, sdl.Keycode(sdl.K_f): 0xE,
+		sdl.Keycode(sdl.K_z): 0xA, sdl.Keycode(sdl.K_x): 0x0, sdl.Keycode(sdl.K_c): 0xB, sdl.Keycode(sdl.K_v): 0xF,
+	}
+
+	if chipKey, ok := mapping[keyCode]; ok {
+		// Ensure your keyboard package uses a slice or array of bools
+		s.Keyboard.Keys[chipKey] = isPressed
+	}
+}
+
+func (s *System) Run(romPath string) error {
+	// 1. Setup
+	if err := s.Display.InitSDL(); err != nil {
+		return fmt.Errorf("failed to init display: %w", err)
+	}
+	defer func() {
+		if err := s.Display.Close(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error closing display: %v\n", err)
+		}
+	}()
+
+	if err := s.LoadROM(romPath); err != nil {
+		return fmt.Errorf("failed to load ROM: %w", err)
+	}
+
+	// 2. Timing logic
+	// We want the CPU to run fast, but Timers/Graphics at 60Hz.
+	cpuHz := 700
+	cpuInterval := time.Second / time.Duration(cpuHz)
+	timerInterval := time.Second / 60
+
+	ticker := time.NewTicker(cpuInterval)
+	defer ticker.Stop()
+
+	lastTimerUpdate := time.Now()
+
+	for range ticker.C {
+		// A. Handle SDL Events
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch t := event.(type) {
+			case *sdl.QuitEvent:
+				return nil
+			case *sdl.KeyboardEvent:
+				s.HandleKeyboard(t)
+			}
+		}
+
+		// B. Step the CPU
+		if err := s.Step(); err != nil {
+			return err
+		}
+
+		// C. Sync Timers and Display to 60Hz
+		if time.Since(lastTimerUpdate) >= timerInterval {
+			s.CPU.UpdateTimers()
+			if err := s.Display.Present(); err != nil {
+				return err
+			}
+			lastTimerUpdate = time.Now()
+		}
+	}
+
+	return nil
 }
