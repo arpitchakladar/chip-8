7091745ab36b3c7652fa8b80a0ea65aed6d10442
Arpit Chakladar <arpitchakladar@proton.me>
2026-04-03 16:00:23 +0530

    Improved the annotation comments

--- docs/static/scripts/code-snippet.js
+++ docs/static/scripts/code-snippet.js
@@ -28,22 +28,22 @@ async function prepareCodeFragments() {
 
 				// 3. Apply Styling
 				popup.style.cssText = `
-				position: absolute;
-				${pos};
-				width: 300px;
-				background: rgba(34, 34, 34, 0.9);
-				border: 2px solid ${color};
-				padding: 15px;
-				box-shadow: 10px 10px 20px rgba(0,0,0,0.5);
-				z-index: 10;
-				text-align: left;
-				border-radius: 8px;
-			`;
+					position: absolute;
+					${pos};
+					width: 300px;
+					background: rgba(34, 34, 34, 0.9);
+					border: 2px solid ${color};
+					padding: 15px;
+					box-shadow: 10px 10px 20px rgba(0,0,0,0.5);
+					z-index: 10;
+					text-align: left;
+					border-radius: 8px;
+				`;
 
 				popup.innerHTML = `
-				<h4 style="margin: 0 0 10px 0; color: ${color}; font-size: 0.8em;">${title}</h4>
-				<p style="font-size: 0.5em; margin: 0; line-height: 1.4;">${text}</p>
-			`;
+					<h5 style="margin: 0 0 10px 0; color: ${color}; font-size: 0.5em;">${title}</h5>
+					<p style="font-size: 0.5em; margin: 0; line-height: 1.4;">${text}</p>
+				`;
 
 				// 4. Move to container and cleanup
 				container.appendChild(popup);
