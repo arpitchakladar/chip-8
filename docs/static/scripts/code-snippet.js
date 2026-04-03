1201309cde4794ddfd56d22e0b6a561fe4c438dd
Arpit Chakladar <arpitchakladar@proton.me>
2026-04-03 21:46:37 +0530

    Now the code snippet also shows the file path at top that is a link to the file in github

--- docs/static/scripts/code-snippet.js
+++ docs/static/scripts/code-snippet.js
@@ -3,6 +3,15 @@ async function prepareCodeFragments() {
 	const containers = document.querySelectorAll("[data-code-snippet]");
 
 	containers.forEach((container) => {
+		// Get all the code snippet viewers
+		const codeSnippetElement = container.querySelector("[data-load-code]");
+		const codePath = codeSnippetElement.getAttribute("data-load-code") || "No file";
+		const codeFilePath = document.createElement("a");
+		codeFilePath.innerHTML = codePath;
+		codeFilePath.className = "secondary code-path";
+		codeFilePath.setAttribute("href", `https://github.com/arpitchakladar/chip-8/blob/master/${codePath}`);
+		container.prepend(codeFilePath);
+
 		// Select only direct children with the data attribute
 		const templates = container.querySelectorAll("[data-code-fragment]");
 
@@ -15,7 +24,6 @@ async function prepareCodeFragments() {
 			const text =
 				el.getAttribute("data-code-fragment-text") || "No text";
 			const color = sequence % 2 === 0 ? "var(--blue)" : "var(--red)";
-			const pos = "";
 
 			indexes.split(",").forEach((fragmentIndex) => {
 				// 2. Create the fragment popup
