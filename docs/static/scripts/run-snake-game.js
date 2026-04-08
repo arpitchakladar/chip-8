122a2c1a3228f70ea0f13ab3140a34294a5af263
Arpit Chakladar <arpitchakladar@proton.me>
2026-04-09 05:07:10 +0530

    added a button to start and restart the chip emulator in the docs slide number 32

--- docs/static/scripts/run-snake-game.js
+++ docs/static/scripts/run-snake-game.js
@@ -1,16 +1,41 @@
+let snakeGameRom = null;
+let snakeGameVM = null;
+let snakeGameButton = null;
+let snakeGameCanvas = null;
+
 async function runSnakeGame() {
-	const canvas = document.querySelector("canvas#snake-game");
-	const vm = await chip_8.Emulator(canvas, 10000);
+	snakeGameVM = await chip_8.Emulator(snakeGameCanvas, 10000);
 
-	const response = await fetch(
-		"https://raw.githubusercontent.com/arpitchakladar/chip-8/refs/heads/master/games/snake.asm",
-	);
-	const asmCode = await response.text();
+	if (!snakeGameRom) {
+		const response = await fetch(
+			"https://raw.githubusercontent.com/arpitchakladar/chip-8/refs/heads/master/games/snake.asm",
+		);
+		const asmCode = await response.text();
 
-	const assembler = await chip_8.Assembler(asmCode);
-	const romData = await assembler.assemble();
+		const assembler = await chip_8.Assembler(asmCode);
+		snakeGameRom = await assembler.assemble();
+	}
 
-	await vm.loadROM(romData);
-	vm.run();
-	await vm.handleKeyboard();
+	await snakeGameVM.loadROM(snakeGameRom);
+	snakeGameVM.run();
+	await snakeGameVM.handleKeyboard();
+}
+
+async function restartSnakeGame() {
+	if (snakeGameVM) {
+		snakeGameVM.destroy();
+		snakeGameVM = null;
+		snakeGameCanvas.setAttribute("width", "640");
+		snakeGameCanvas.setAttribute("height", "320");
+	} else {
+		snakeGameButton.innerHTML = "RESTART";
+	}
+	await runSnakeGame();
+	snakeGameCanvas.focus();
+}
+
+async function setupSnakeGame() {
+	snakeGameButton = document.querySelector("button#snake-game-button");
+	snakeGameCanvas = document.querySelector("canvas#snake-game");
+	snakeGameButton.addEventListener("click", restartSnakeGame);
 }
