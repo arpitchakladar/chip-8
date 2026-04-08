15fb92f756712cab41740633755e00f3d5a78d1b
Arpit Chakladar <arpitchakladar@proton.me>
2026-04-08 21:41:09 +0530

    Running the game in a canvas inside the presentation

---
+++ docs/static/scripts/run-snake-game.js
@@ -0,0 +1,15 @@
+async function runSnakeGame() {
+	const canvas = document.querySelector("canvas#snake-game");
+	const vm = new chip_8.Emulator(canvas, 100000);
+
+	const response = await fetch(
+		"https://raw.githubusercontent.com/arpitchakladar/chip-8/refs/heads/master/games/snake.asm",
+	);
+	const asmCode = await response.text();
+
+	const assembler = new chip_8.Assembler(asmCode);
+	const romData = assembler.assemble();
+
+	vm.loadROM(romData);
+	vm.run();
+}
